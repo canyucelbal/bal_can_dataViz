c46174c9e898779877bcf3fdb3163ed22433c997
Can Yucel Bal <canyucelbal@Cans-MacBook-Pro.local>
2019-12-06 11:11:45 -0500

    finished version of HW

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # bal_can_dataViz
+
+## Getting Started 
+Data vizualizations are created via Python. Data is based on IOC Research and Reference Service (Published by Guardian's datablog)
+You can visually follow the structure via HTML.
+
+### Prequisites 
+You can use Python for free to open .py files
+You can use free code editors to open HTML and CSS such as Sublime Text
+
+### Author 
+1. Can Yucel Bal - Python Data Visualization, Design, Html, Css
---
+++ data/pt_dataViz_6.py
@@ -0,0 +1,52 @@
+import csv
+import matplotlib.pyplot as plt
+
+golds = []
+silvers = []
+bronzes = []
+
+categories = []
+
+with open('data/bal_can_olympics_data.csv') as csvfile:
+		reader = csv.reader(csvfile)
+		line_count = 0 
+
+		for row in reader:
+			if line_count is 0:
+				print("this is the first row in spreadsheet")
+				categories.append(row)
+				line_count += 1
+			
+			else:
+				if row[4] == "CAN":	
+					if row[7] == "Gold":
+						print("won a gold medal")
+						golds.append(row[7])	
+
+					elif row[7] == "Silver":
+						print("won a silver medal")	
+						silvers.append(row[7])
+
+					else:
+						print("won a bronze medal")
+						bronzes.append(row[7])
+
+				print(line_count)
+				line_count = 1	
+
+print(len(golds))
+print(len(silvers))
+print(len(bronzes))
+
+labels = ["Gold", "Silver", "Bronze"]
+sizes = [len(golds), len(silvers), len(bronzes)]
+color = ['gold', 'silver', 'darkgoldenrod']
+explode = (0.02, 0.02, 0.02)
+
+plt.pie(sizes, explode=explode, colors=color, autopct='%.1f%%', shadow=True, startangle=140)
+
+plt.axis('equal')
+
+plt.legend(labels, loc=1)
+plt.title("Percentage Of Gold, Silver, Bronze medals won by Canada (1924-2014)")
+plt.show()
